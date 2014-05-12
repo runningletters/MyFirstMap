beee1c9387940d5337e123512c63f0daf8d80238
runningletters <newlylife@foxmail.com>
2014-05-13 00:40:28 +0800

    开发基本的地图展示功能
    开发基本的地图展示功能

---
+++ src/com/hekangping/myfirstmap/MainActivity.java
@@ -0,0 +1,121 @@
+package com.hekangping.myfirstmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.MapController;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.platform.comapi.basestruct.GeoPoint;
+
+public class MainActivity extends Activity {
+
+	final static String TAG = "MainActivity";
+	/**
+	 * MapView 是地图主控件
+	 */
+	private MapView mMapView = null;
+	/**
+	 * 用MapController完成地图控制
+	 */
+	private MapController mMapController = null;
+
+	BMapManager mBMapMan = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/**
+		 * 由于MapView在setContentView()中初始化,所以它需要在BMapManager初始化之后
+		 */
+		mBMapMan = new BMapManager(getApplication());
+		mBMapMan.init(null);
+
+		setContentView(R.layout.activity_main);
+
+		// 注意：请在试用setContentView前初始化BMapManager对象，否则会报错
+		setContentView(R.layout.activity_main);
+		mMapView = (MapView) findViewById(R.id.bmapsView);
+		mMapView.setBuiltInZoomControls(true);
+		// 设置启用内置的缩放控件
+		MapController mMapController = mMapView.getController();
+		// 得到mMapView的控制权,可以用它控制和驱动平移和缩放
+		GeoPoint point = new GeoPoint((int) (39.915 * 1E6),
+				(int) (116.404 * 1E6));
+		// 用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)
+		mMapController.setCenter(point);// 设置地图中心点
+		mMapController.setZoom(12);// 设置地图zoom级别
+	}
+
+	@Override
+	protected void onDestroy() {
+		mMapView.destroy();
+		if (mBMapMan != null) {
+			mBMapMan.destroy();
+			mBMapMan = null;
+		}
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		mMapView.onPause();
+		if (mBMapMan != null) {
+			mBMapMan.stop();
+		}
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		mMapView.onResume();
+		if (mBMapMan != null) {
+			mBMapMan.start();
+		}
+		super.onResume();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
