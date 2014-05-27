7f83dad6ecc8b6b663211815bd842e92e0bf79c2
runningletters <newlylife@foxmail.com>
2014-05-27 08:12:24 +0800

    定位
    定位

--- src/com/hekangping/myfirstmap/MainActivity.java
+++ src/com/hekangping/myfirstmap/MainActivity.java
@@ -1,17 +1,34 @@
 package com.hekangping.myfirstmap;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.LocationData;
 import com.baidu.mapapi.map.MapController;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationOverlay;
+import com.baidu.mapapi.map.MyLocationOverlay.LocationMode;
+import com.baidu.mapapi.map.PopupClickListener;
+import com.baidu.mapapi.map.PopupOverlay;
 import com.baidu.platform.comapi.basestruct.GeoPoint;
 
 public class MainActivity extends Activity {
@@ -20,13 +37,34 @@ public class MainActivity extends Activity {
 	/**
 	 * MapView 是地图主控件
 	 */
-	private MapView mMapView = null;
+	private MyLocationMapView mMapView = null;
 	/**
 	 * 用MapController完成地图控制
 	 */
 	private MapController mMapController = null;
 
 	BMapManager mBMapMan = null;
+
+	private enum E_BUTTON_TYPE {
+		LOC, COMPASS, FOLLOW
+	}
+
+	private E_BUTTON_TYPE mCurBtnType;
+
+	// 定位相关
+	LocationClient mLocClient;
+	LocationData locData = null;
+	public MyLocationListenner myListener = new MyLocationListenner();
+
+	// 定位图层
+	LocationOverlay myLocationOverlay = null;
+	// 弹出泡泡图层
+	private PopupOverlay pop = null;// 弹出泡泡图层，浏览节点时使用
+	private TextView popupText = null;// 泡泡view
+
+	Button requestLocButton = null;
+	boolean isRequest = false;// 是否手动触发请求定位
+	boolean isFirstLoc = true;// 是否首次定位
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,18 +77,96 @@ public class MainActivity extends Activity {
 
 		setContentView(R.layout.activity_main);
 
+		requestLocButton = (Button) findViewById(R.id.button1);
+		mCurBtnType = E_BUTTON_TYPE.LOC;
+		OnClickListener btnClickListener = new OnClickListener() {
+			public void onClick(View v) {
+				switch (mCurBtnType) {
+				case LOC:
+					// 手动定位请求
+					requestLocClick();
+					break;
+				case COMPASS:
+					myLocationOverlay.setLocationMode(LocationMode.NORMAL);
+					requestLocButton.setText("定位");
+					mCurBtnType = E_BUTTON_TYPE.LOC;
+					break;
+				case FOLLOW:
+					myLocationOverlay.setLocationMode(LocationMode.COMPASS);
+					requestLocButton.setText("罗盘");
+					mCurBtnType = E_BUTTON_TYPE.COMPASS;
+					break;
+				}
+			}
+		};
+		requestLocButton.setOnClickListener(btnClickListener);
+
 		// 注意：请在试用setContentView前初始化BMapManager对象，否则会报错
 		setContentView(R.layout.activity_main);
-		mMapView = (MapView) findViewById(R.id.bmapsView);
+		mMapView = (MyLocationMapView) findViewById(R.id.bmapsView);
 		mMapView.setBuiltInZoomControls(true);
+
 		// 设置启用内置的缩放控件
 		MapController mMapController = mMapView.getController();
+		/*
 		// 得到mMapView的控制权,可以用它控制和驱动平移和缩放
 		GeoPoint point = new GeoPoint((int) (39.915 * 1E6),
 				(int) (116.404 * 1E6));
 		// 用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)
 		mMapController.setCenter(point);// 设置地图中心点
+		
 		mMapController.setZoom(12);// 设置地图zoom级别
+		mMapController.enableClick(true);
+*/
+		// 创建 弹出泡泡图层
+		createPaopao();
+
+		// 定位初始化
+		mLocClient = new LocationClient(this);
+		locData = new LocationData();
+		mLocClient.registerLocationListener(myListener);
+		LocationClientOption option = new LocationClientOption();
+		option.setOpenGps(true);// 打开gps
+		option.setCoorType("bd09ll"); // 设置坐标类型
+		option.setScanSpan(1000);
+		mLocClient.setLocOption(option);
+		mLocClient.start();
+
+		// 定位图层初始化
+		myLocationOverlay = new LocationOverlay(mMapView);
+		// 设置定位数据
+		myLocationOverlay.setData(locData);
+		// 添加定位图层
+		mMapView.getOverlays().add(myLocationOverlay);
+		myLocationOverlay.enableCompass();
+		// 修改定位数据后刷新图层生效
+		mMapView.refresh();
+
+	}
+
+	/**
+	 * 创建弹出泡泡图层
+	 */
+	public void createPaopao() {
+		popupText = (TextView) findViewById(R.id.textcache);
+		// 泡泡点击响应回调
+		PopupClickListener popListener = new PopupClickListener() {
+			@Override
+			public void onClickedPopup(int index) {
+				Log.v("click", "clickapoapo");
+			}
+		};
+		pop = new PopupOverlay(mMapView, popListener);
+		MyLocationMapView.pop = pop;
+	}
+
+	/**
+	 * 手动触发一次定位请求
+	 */
+	public void requestLocClick() {
+		Toast.makeText(MainActivity.this, "正在定位……", Toast.LENGTH_SHORT).show();
+		isRequest = true;
+		mLocClient.requestLocation();
 	}
 
 	@Override
@@ -79,6 +195,19 @@ public class MainActivity extends Activity {
 			mBMapMan.start();
 		}
 		super.onResume();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mMapView.onSaveInstanceState(outState);
+
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		mMapView.onRestoreInstanceState(savedInstanceState);
 	}
 
 	@Override
@@ -118,4 +247,99 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	// 继承MyLocationOverlay重写dispatchTap实现点击处理
+	public class LocationOverlay extends MyLocationOverlay {
+
+		public LocationOverlay(MapView mapView) {
+			super(mapView);
+		}
+
+		@Override
+		protected boolean dispatchTap() {
+			// 处理点击事件,弹出泡泡
+			popupText.setBackgroundResource(R.drawable.popup);
+			popupText.setText("我的位置");
+			pop.showPopup(BMapUtil.getBitmapFromView(popupText), new GeoPoint(
+					(int) (locData.latitude * 1e6),
+					(int) (locData.longitude * 1e6)), 8);
+			return true;
+		}
+
+	}
+
+	/**
+	 * 定位SDK监听函数
+	 */
+	public class MyLocationListenner implements BDLocationListener {
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			if (location == null)
+				return;
+
+			locData.latitude = location.getLatitude();
+			locData.longitude = location.getLongitude();
+			// 如果不显示定位精度圈，将accuracy赋值为0即可
+			locData.accuracy = location.getRadius();
+			// 此处可以设置 locData的方向信息, 如果定位 SDK 未返回方向信息，用户可以自己实现罗盘功能添加方向信息。
+			locData.direction = location.getDerect();
+			// 更新定位数据
+			myLocationOverlay.setData(locData);
+			// 更新图层数据执行刷新后生效
+			mMapView.refresh();
+			// 是手动触发请求或首次定位时，移动到定位点
+			if (isRequest || isFirstLoc) {
+				// 移动地图到定位点
+				Log.d("LocationOverlay", "receive location, animate to it");
+				mMapController.animateTo(new GeoPoint(
+						(int) (locData.latitude * 1e6),
+						(int) (locData.longitude * 1e6)));
+				isRequest = false;
+				myLocationOverlay.setLocationMode(LocationMode.FOLLOWING);
+				requestLocButton.setText("跟随");
+				mCurBtnType = E_BUTTON_TYPE.FOLLOW;
+			}
+			// 首次定位完成
+			isFirstLoc = false;
+		}
+
+		public void onReceivePoi(BDLocation poiLocation) {
+			if (poiLocation == null) {
+				return;
+			}
+		}
+	}
+
+}
+
+/**
+ * 继承MapView重写onTouchEvent实现泡泡处理操作
+ * 
+ * @author hejin
+ * 
+ */
+class MyLocationMapView extends MapView {
+	static PopupOverlay pop = null;// 弹出泡泡图层，点击图标使用
+
+	public MyLocationMapView(Context context) {
+		super(context);
+	}
+
+	public MyLocationMapView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public MyLocationMapView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (!super.onTouchEvent(event)) {
+			// 消隐泡泡
+			if (pop != null && event.getAction() == MotionEvent.ACTION_UP)
+				pop.hidePop();
+		}
+		return true;
+	}
 }
